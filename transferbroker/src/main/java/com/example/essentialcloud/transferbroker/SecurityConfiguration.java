6b3278198d967115bf6071716c77a0ee7d67c5ad
Karl Nicholas <karl.nicholas@outlook.com>
2023-12-23 16:03:06 -0500

    add GrantedAuthority filter

--- transferbroker/src/main/java/com/example/essentialcloud/transferbroker/SecurityConfiguration.java
+++ transferbroker/src/main/java/com/example/essentialcloud/transferbroker/SecurityConfiguration.java
@@ -9,10 +9,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.access.intercept.AuthorizationFilter;
-import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import
 
 @Configuration
 @EnableWebSecurity
@@ -21,22 +17,7 @@ public class SecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
                 .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
-        http.addFilterAfter((request, response, chain) -> {
-
-        }, UsernamePasswordAuthenticationFilter.class);
         return http.sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
-    }
-
-    static class MyFilter extends UsernamePasswordAuthenticationFilter {
-
-        /**
-         * Creates an instance.
-         *
-         * @param authorizationManager the {@link AuthorizationManager} to use
-         */
-        public MyFilter(AuthorizationManager<HttpServletRequest> authorizationManager) {
-            super(authorizationManager);
-        }
     }
 }
